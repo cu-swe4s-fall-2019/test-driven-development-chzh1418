4edd60e84ae25caf74c1aa18fdec4fa48ec63d08
chzh1418 <chzh1418@colorado.edu>
2019-09-27 00:39:32 +0000

    Modify get_data and test run

--- get_data.py
+++ get_data.py
@@ -18,16 +18,18 @@ def read_stdin_col(col_num):
         Return 'None' if cannot be extracted
     """
     # Check input file and range of column
-    if sys.stdin is None:
+    if col_num < 0:
+        raise ValueError('Column number should be positive')
+    try:
+        f = open(sys.stdin)
+    except FileNotFoundError:
+        raise FileNotFoundError('File not found')
         return None
-
     A = []
-    for line in sys.stdin:
-        try:
-            Array = [int(x) for x in line.split()]
-            A.append(int(Array[col_num]))
-        except IndexError:
-            return None
-        except ValueError:
-            return None
+    for line in f:
+        Array = line.strip().split()
+        if col_num >= len(Array):
+            raise ValueError('Index out of bound')
+        A.append(int(Array[col_num]))
+    f.close()
     return A
